697c449332c07577e88a16744257874a9752c2b0
anorv <69910278+anorv@users.noreply.github.com>
2020-12-01 15:38:02 +0200

    e-shop

--- assets/js/shopCart.js
+++ assets/js/shopCart.js
@@ -1,68 +1,56 @@
 let cart = document.querySelectorAll(".add_cart");
 
-
 let product = [
-    {
-        name: "1 padžas",
-        tag: "padazas1",
-        price: 7,
-        inCart: 0
-
-    },
-    {
-        name: "2 padžas",
-        tag: "padazas2",
-        price: 7,
-        inCart: 0
-
-    },
-    {
-        name: "3 padžas",
-        tag: "padazas3",
-        price: 7,
-        inCart: 0
-
-    },
-    {
-        name: "4 padžas",
-        tag: "padazas4",
-        price: 7,
-        inCart: 0
-
-    },
-    {
-        name: "5 padžas",
-        tag: "padazas5",
-        price: 7,
-        inCard: 0
-
-    },
-
+  {
+    name: "1 padžas",
+    tag: "souce1",
+    price: 7,
+    inCart: 0,
+  },
+  {
+    name: "2 padžas",
+    tag: "souce2",
+    price: 7,
+    inCart: 0,
+  },
+  {
+    name: "3 padžas",
+    tag: "souce3",
+    price: 7,
+    inCart: 0,
+  },
+  {
+    name: "4 padžas",
+    tag: "souce4",
+    price: 7,
+    inCart: 0,
+  },
+  {
+    name: "5 padžas",
+    tag: "souce5",
+    price: 7,
+    inCard: 0,
+  },
 ];
 
 // kad paspaudus mygtuka deti i krepseli veiktu
 for (let i = 0; i < cart.length; i++) {
-  cart[i].addEventListener("click", () => {
+  cart[i].addEventListener("click", (e) => {
     cartNr(product[i]);
+    e.preventDefault();
+    totalCost(product[i]);
   });
 }
 
-
 // sukurti funkcija kad perkrovus puslapi krepselio skaicius nepersikrautu
-function onLoadCartNr (){
-    let productNumber = localStorage.getItem("cartNr");
+function onLoadCartNr() {
+  let productNumber = localStorage.getItem("cartNr");
 
-    if(productNumber){
-        document.querySelector(".cart_div").textContent = productNumber;
-
-    }
-
-}
-
-
-
-
-
+  if (productNumber) {
+    document.querySelector(".cart_div").textContent = productNumber;
+    document.querySelector(".cart_div_second").textContent = productNumber;
+  };
+};
 
 function cartNr(product) {
   // kad i google localstorage idetume prekes
@@ -74,41 +62,79 @@ function cartNr(product) {
     localStorage.setItem("cartNr", productNumber + 1);
     // kad navbare 0 butu lygus papsaudus deti i krepseli toks pat
     document.querySelector(".cart_div").textContent = productNumber + 1;
+    document.querySelector(".cart_div_second").textContent = productNumber + 1;
   } else {
     // kitu atvrju lieka 1
     localStorage.setItem("cartNr", 1);
     // kad navbare 0 butu lygus papsaudus deti i krepseli toks pat
     document.querySelectorAll(".cart_div").textContent = 1;
-  }
+    document.querySelectorAll(".cart_div_second").textContent = 1;
+  };
 
   setItems(product);
-}
+};
+
+function setItems(product) {
+  let cartItems = localStorage.getItem("product");
+  cartItems = JSON.parse(cartItems);
+
+  if (cartItems != null) {
+    if (cartItems[product.tag] == undefined) {
+      cartItems = {
+        ...cartItems,
+        [product.tag]: product,
+      };
+    };
+    cartItems[product.tag].inCart += 1;
+  } else {
+    product.inCart = 1;
+    cartItems = {
+      [product.tag]: product,
+    };
+  };
+
+  localStorage.setItem("product", JSON.stringify(cartItems));
+};
+
+// Funkcija suskaiciuoti bendra suma 
+function totalCost (product){
+    let cardCost = localStorage.getItem("totalCost");
 
 
+    if (cardCost != null){
+         // Kadangi is localStorage mums grazina tik strings, pakeiciame su parseIn i number
+    cardCost = parseInt(cardCost);
+ localStorage.setItem("totalCost", cardCost + product.price);
+    }else {
+        localStorage.setItem("totalCost", product.price);
+    };
 
-function setItems(product){
-    let cartItems = localStorage.getItem ("product");
-     cartItems = JSON.parse(cartItems); 
+};
 
-      if(cartItems != null){
-          if(cartItems[product.tag] == undefined){
-              cartItems = {
-                  ...cartItems,
-                  [product.tag]: product
-              }
-          }
-          cartItems[product.tag].inCart += 1;
-      }else{
-        product.inCart = 1;
-        cartItems = {
-           [product.tag]: product
-       }
+// kad duomenis is mano localShop atspausdintu i card.html
+// function displayCard() {
+//    let cartItems = localStorage.getItem("product");
+//   //  kai is json pajamame localStor turime ji paversti i narmalu js
+//   cartItems = JSON.parse(cartItems); 
+// console.log(cartItems);
+// let proctuctContainer = document.querySelector(".product_container");
+// if(cartItems && proctuctContainer){
 
-      }
+//   proctuctContainer.innerHTM = '';
+//   Object.values(cartItems).map(item =>{
+//     proctuctContainer.innerHTML += 
+//     `<div class="products">
+    
+//     <img src="assets/img/${item.tag}.jpg>
+//     <span>${item.name}</span
+//     </div>`
 
-    localStorage.setItem("product", JSON.stringify(cartItems));
+//   });
 
-}
 
+// };
+ 
+// };
 // paleidziame funkcija, kad uzsikrovus puslapiui krepselio nr sutaptu su localStorage nr
-onLoadCartNr ();
+onLoadCartNr(); 
+displayCard();
